1248c084725077774544173fafc68a6288375253
brady <brady@moonjoy.net>
2016-08-29 09:58:07 +0700

    update

--- shell/init.sh
+++ shell/init.sh
@@ -30,6 +30,16 @@ web:
   volumes:
     - ${WEB_DIR}:/var/www
 
+trans:
+  build: ${DOCKER_DIR}/trans
+  ports:
+    - `expr ${PORT_OFFSET} + 80`:80
+  links:
+    - mysql
+    - redis
+  volumes:
+    - ${TRANS_DIR}:/var/www
+
 " > ${CURRENT_DIR}/docker-compose.yml
 
 # 获取当前用户的user_id与group_id, 以便在dockerfile中动态配置运行的用户
